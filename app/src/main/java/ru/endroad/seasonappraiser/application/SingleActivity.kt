0cc58be18e2943e9c86e87196966e8c8b8be6972
dvoljatik <o.dvoljatik@cft.ru>
2020-05-18 20:23:16 +0700

    Добавил основное активити-приложение

---
+++ app/src/main/java/ru/endroad/seasonappraiser/application/SingleActivity.kt
@@ -0,0 +1,8 @@
+package ru.endroad.seasonappraiser.application
+
+import ru.endroad.camp.activity.CampActivity
+import ru.endroad.seasonappraiser.R
+
+class SingleActivity : CampActivity() {
+	override val layout = R.layout.base_layout
+}
