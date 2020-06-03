615d13fade1efad06313aa8efac846f7f503b4cf
dvoljatik <o.dvoljatik@cft.ru>
2020-06-03 22:28:56 +0700

    Заменил скрин детализации сезона

--- app/src/main/java/ru/endroad/seasonappraiser/routing/FeedSeriesRouterImpl.kt
+++ app/src/main/java/ru/endroad/seasonappraiser/routing/FeedSeriesRouterImpl.kt
@@ -1,7 +1,7 @@
 package ru.endroad.seasonappraiser.routing
 
 import androidx.fragment.app.FragmentManager
-import ru.endroad.feature.detail.view.SeriesDetailFragment
+import ru.endroad.feature.detail.view.SeriesDetailMatrixFragment
 import ru.endroad.feature.feed.router.FeedSeriesRouter
 import ru.endroad.navigation.routing.FragmentRouting
 import ru.endroad.navigation.routing.changeRoot
@@ -9,5 +9,5 @@ import ru.endroad.seasonappraiser.R
 
 class FeedSeriesRouterImpl(override val fragmentManager: FragmentManager) : FeedSeriesRouter, FragmentRouting {
 
-	override fun openSeries(seriesId: String) = changeRoot(SeriesDetailFragment.newInstance(seriesId), R.id.root)
+	override fun openSeries(seriesId: String) = changeRoot(SeriesDetailMatrixFragment.newInstance(seriesId), R.id.root)
 }
