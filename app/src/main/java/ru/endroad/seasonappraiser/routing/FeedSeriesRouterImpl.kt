bf6e01da9c293fea380ec35d150a758818a8d677
dvoljatik <o.dvoljatik@cft.ru>
2020-06-03 23:44:24 +0700

    Заменил смену фрагмента на открытие

--- app/src/main/java/ru/endroad/seasonappraiser/routing/FeedSeriesRouterImpl.kt
+++ app/src/main/java/ru/endroad/seasonappraiser/routing/FeedSeriesRouterImpl.kt
@@ -4,10 +4,10 @@ import androidx.fragment.app.FragmentManager
 import ru.endroad.feature.detail.view.SeriesDetailMatrixFragment
 import ru.endroad.feature.feed.router.FeedSeriesRouter
 import ru.endroad.navigation.routing.FragmentRouting
-import ru.endroad.navigation.routing.changeRoot
+import ru.endroad.navigation.routing.forwardTo
 import ru.endroad.seasonappraiser.R
 
 class FeedSeriesRouterImpl(override val fragmentManager: FragmentManager) : FeedSeriesRouter, FragmentRouting {
 
-	override fun openSeries(seriesId: String) = changeRoot(SeriesDetailMatrixFragment.newInstance(seriesId), R.id.root)
+	override fun openSeries(seriesId: String) = forwardTo(SeriesDetailMatrixFragment.newInstance(seriesId), R.id.root)
 }
