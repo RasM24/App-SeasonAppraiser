b5258fa1ced62d803a34b945f5ff5007c0cd3fde
dvoljatik <o.dvoljatik@cft.ru>
2020-06-05 23:24:59 +0700

    fixup! Добавил use case для поиска сериалов

--- shared-serial/src/main/java/ru/endroad/shared/serial/di/SharedSerialModule.kt
+++ shared-serial/src/main/java/ru/endroad/shared/serial/di/SharedSerialModule.kt
@@ -2,14 +2,12 @@ package ru.endroad.shared.serial.di
 
 import org.koin.dsl.module
 import org.koin.experimental.builder.single
-import ru.endroad.shared.serial.domain.GetBestSerialsUseCase
-import ru.endroad.shared.serial.domain.GetPopularSerialsUseCase
-import ru.endroad.shared.serial.domain.GetRecommendedSerialsUseCase
-import ru.endroad.shared.serial.domain.GetSerialDetailUseCase
+import ru.endroad.shared.serial.domain.*
 
 val sharedSerialModule = module {
 	single<GetPopularSerialsUseCase>()
 	single<GetRecommendedSerialsUseCase>()
 	single<GetBestSerialsUseCase>()
 	single<GetSerialDetailUseCase>()
+	single<GetSerialListByNameUseCase>()
 }
